77ee3176835341b80ffae7e54e4cabcd4d2b0841
Mike <mikeadkison@gmail.com>
2015-10-06 19:07:37 -0400

    add mouse press and mouse release detectors

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,28 +5,53 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
+import javafx.scene.shape.Line;
+import javafx.scene.input.MouseEvent;
+
 public class HelloWorld extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
+    private boolean mouseDown = false;
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+
+        Line line = new Line();
+        line.setStartX(0.0f);
+        line.setStartY(0.0f);
+        line.setEndX(100.0f);
+        line.setEndY(100.0f);
+        
+
+
+
+        primaryStage.setTitle("Draw Shapes");
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+
+        root.getChildren().add(line);
         primaryStage.setScene(new Scene(root, 300, 250));
+
+        root.addEventFilter(MouseEvent.MOUSE_PRESSED, 
+                    new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            System.out.println("MOUSE_PRESSED: " + "x: " + String.valueOf(event.getSceneX()) + " ,y: " + String.valueOf(event.getSceneY()));
+                            mouseDown = true;
+                        }
+                    });
+
+        root.addEventFilter(MouseEvent.MOUSE_RELEASED, 
+                    new EventHandler<MouseEvent>() {
+                        @Override
+                        public void handle(MouseEvent event) {
+                            System.out.println("MOUSE_RELEASED: " + "x: " + String.valueOf(event.getSceneX()) + " ,y: " + String.valueOf(event.getSceneY()));
+                            mouseDown = false;
+                        }
+                    });
+
         primaryStage.show();
     }
 }
